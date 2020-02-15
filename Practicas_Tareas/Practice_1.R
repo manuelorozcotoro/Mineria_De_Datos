419e4e51946a4fc3d940f48266debf2306a0c856
alejandrodominguezt <60455774+alejandrodominguezt@users.noreply.github.com>
2020-02-14 19:57:30 -0800

    Create Practice_1.R

---
+++ Practicas_Tareas/Practice_1.R
@@ -0,0 +1,26 @@
+# Practice 1
+
+# 1. Initialize sample size
+N<-10
+sample(-1:1)
+# 2. Initialize counter
+
+counter<-1
+# 3. loop for(i in rnorm(size))
+
+for(i in rnorm(-1:1)){
+# 4. Check if the iterated variable falls
+  print(i)
+  
+  # 5. Increase counter if the condition is true  
+  while(TRUE)
+  {
+    counter<-counter+1
+
+  }
+  
+}
+
+# 6. return a result <- counter / N
+result<- counter / N
+print(result)
