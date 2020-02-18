dfcb66ecc785e009cd85b27abd81774525913d87
alejandrodominguezt <60455774+alejandrodominguezt@users.noreply.github.com>
2020-02-17 18:27:26 -0800

    Create Practice_2.R

---
+++ Practicas_Tareas/Practice_2.R
@@ -0,0 +1,38 @@
+# Practice find 20 more funtions in R and make an example of it.
+
+# variable for example
+dt <- cars
+
+#1. length()
+length(dt)
+
+#2. abs() 
+abs(dt)
+
+#3. exp()
+exp(dt)
+
+#4. mean()
+mean(dt)
+
+#5. sum()
+sum(dt)
+
+#6. min()
+min(dt)
+
+#7. max()
+max(dt)
+
+#8. sd()
+sd(dt)
+
+#9. coef()
+coef(dt)
+
+#10. factorial()
+factorial(dt)
+
+#11. 
+
+#12.
