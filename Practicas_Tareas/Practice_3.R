00208d77daef4b0c36458c93a5a2663763864408
alejandrodominguezt <60455774+alejandrodominguezt@users.noreply.github.com>
2020-02-18 23:19:06 -0800

    Update Practice_3.R

--- Practicas_Tareas/Practice_3.R
+++ Practicas_Tareas/Practice_3.R
@@ -1,38 +1,10 @@
 #Practice 3
 
-Scenario: You are a Data Scientist working for a consulting firm.
-One of your colleagues from the Auditing Department has asked you
-to help them assess the financial statement of organization X.
-
-You have been supplied with two vector of data: mounthly revenue and
-expenses for the financial year in quiestion. Your task is to calculate
-the following financial matrics:
-  
-  - profit for each mounth
-- profit after tax for each month (the tax rate is 30%)
-- profit margin for each month - equal to profit after tax divided by revenue
-- good months - where the profit after tax was greater than the mean for the year
-- bad months - where the profit after tax was less then the mean for years
-- the best month - where the profit after tax was max for the year
-- the worst month - where the profit after tax was min for the year
-
-All results need to be presented as vectors.
-
-Results for dollar values need to be calculate with $0.01 precision, but need to be
-presented in Units of $1,000(i.e. 1k) with no decimal point.
-
-Results for the profit margin ratio needed to be presented in units of % with no
-decimal points.
-
-Note: Your collegue has warned you that it is okay for tax for any given month to be
-negative (in accounting terms, negative tax translates into a deferred tax asset).
-
-Hint 1
-Use:
-  round()
-mean()
-max()
-min()
+#Use:
+ # round()
+ # mean()
+|# max()
+|# min()
 
 #Data
 revenue <- c(14574.49, 7606.46, 8611.41, 9175.41, 8058.65, 8105.44, 11496.28, 9766.09, 10305.32, 14379.96, 10713.97, 15433.50)
