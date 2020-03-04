00b07ee1ee4f2fdd098087097a77988e076debe7
alejandrodominguezt <60455774+alejandrodominguezt@users.noreply.github.com>
2020-03-04 10:30:55 -0800

    Create Practice_5.R
    Part 1 of Practice 5 Filter Countries

---
+++ Practicas_Tareas/Practice_5.R
@@ -0,0 +1,23 @@
+#Practice 5
+
+#Filter countries by Low income
+
+stats[stats$Income.Group=="Low income",]
+
+#Filter countries by Lower middle income
+
+stats[stats$Income.Group=="Lower middle income",]
+
+#Filter countries by Upper middle income
+
+stats[stats$Income.Group=="Upper middle income",]
+
+#Filter by countrie Malta
+
+stats[stats$Country.Name=="Malta",]
+
+#Filter by countrie Qatar
+
+#Filter by countrie Netherlands
+
+#Filter by countrie Norway
